1d75eedc113c8e9d964af36d3535710e32e50a45
Charis153 <isabiryem916@gmail.com>
2026-09-18 17:16:21 +0300

    Modified the e-commerce logo

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import useCartStore from '../../store/cartStore'
 import useAuthStore from '../../store/authStore'
-import logo from '../assets/logo.jpg'; // adjust the path to wherever your file lives
+import logo from '../assets/logo.jpeg'; // adjusted the file path for the logo.
 
 
 export default function Navbar() {
