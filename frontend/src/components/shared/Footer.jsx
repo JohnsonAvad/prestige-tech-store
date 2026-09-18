b37aaa07b80bb951a01283ae5bd461a2cea36388
Charis153 <isabiryem916@gmail.com>
2026-09-18 13:25:57 +0300

    Changed the footer background color again

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 mt-16">
+    <footer className="bg-gray-950 mt-16">
 
       <div className="max-w-7xl mx-auto px-4 md:px-6 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
